75e5269f356b26fc921137b15699dce54845ea07
SOTGGON <maoxiehui@outlook.com>
2024-01-03 18:59:41 +0100

    PlayMode funktuon, switch mellan In order, Random och Repeat

--- audioplayer/Form1.Designer.cs
+++ audioplayer/Form1.Designer.cs
@@ -89,9 +89,9 @@
             // 
             this.button4.Location = new System.Drawing.Point(372, 508);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.Size = new System.Drawing.Size(80, 35);
             this.button4.TabIndex = 3;
-            this.button4.Text = "In order";
+            this.button4.Text = "Play In Order";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
--- audioplayer/Form1.cs
+++ audioplayer/Form1.cs
@@ -22,10 +22,11 @@ namespace audioplayer
         string[] files, paths;
         string localSelectPath;
         List<string> localSelectPathList = new List<string>();
+        Random random = new Random();
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex >= 0)
+            /*if (listBox1.SelectedIndex >= 0)
             {
                 if (listBox1.SelectedIndex == 0)
                 {
@@ -35,7 +36,9 @@ namespace audioplayer
                 {
                     listBox1.SelectedIndex = listBox1.SelectedIndex - 1;
                 }
-            }
+            }*/
+
+            playMode(true);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -56,19 +59,35 @@ namespace audioplayer
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
+            /*if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
             {
                 listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
             }
             else
             {
                 listBox1.SelectedIndex = 0;
-            }
+            }*/
+
+            playMode(false);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            if (button4.Text == "Play In Order")
+            {
+                button4.Text = "Play Random";
+                button4.Width = 80;
+            }
+            else if (button4.Text == "Play Random")
+            {
+                button4.Text = "Repeat Single";
+                button4.Width = 83;
+            }
+            else
+            {
+                button4.Text = "Play In Order";
+                button4.Width = 80;
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -163,6 +182,83 @@ namespace audioplayer
         {
 
         }
+
+        #region Playmode
+        public void playMode(bool mode)
+        {
+            if (button4.Text == "Play In Order")
+            {
+                if (mode)
+                {
+                    if (listBox1.SelectedIndex >= 0)
+                    {
+                        if (listBox1.SelectedIndex == 0)
+                        {
+                            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                        }
+                        else
+                        {
+                            listBox1.SelectedIndex = listBox1.SelectedIndex - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (listBox1.SelectedIndex < listBox1.Items.Count - 1)
+                    {
+                        listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+                    }
+                    else
+                    {
+                        listBox1.SelectedIndex = 0;
+                    }
+                }
+                
+            }
+            else if (button4.Text == "Play Random")
+            {
+                
+                int r = random.Next(0, listBox1.Items.Count);
+
+                if (r == listBox1.SelectedIndex)
+                {
+                    if ((r+1) == listBox1.Items.Count)
+                    {
+                        r--;
+                    }
+                    else
+                    {
+                        r++;
+                    }
+                }
+
+                if (localSelectPathList.Count > 0)
+                {
+                    axWindowsMediaPlayer1.URL = localSelectPathList[listBox1.SelectedIndex = r];
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.URL = paths[listBox1.SelectedIndex = r];
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+            }
+            else if (button4.Text == "Repeat Single")
+            {
+                if (localSelectPathList.Count > 0)
+                {
+                    axWindowsMediaPlayer1.URL = localSelectPathList[listBox1.SelectedIndex];
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+                else
+                {
+                    axWindowsMediaPlayer1.URL = paths[listBox1.SelectedIndex];
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                }
+            }
+        }
+
+        #endregion
 
     }
 }
